1f608b54a2030385141da1bc2882a239b9138aa3
Sergey <sergey@spendesk.com>
2021-07-30 23:35:33 +0200

    menu

--- components/TopMenu/TopMenu.tsx
+++ components/TopMenu/TopMenu.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import cx from "classnames";
 
 export const TopMenu = () => {
+  const ref = useRef(null);
+  const [isContentMenu, setIsContentMenu] = useState(false);
+  const onScroll = (e) => {
+    setIsContentMenu(
+      e.target.scrollingElement.scrollTop >
+        e.target.scrollingElement.clientHeight
+    );
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll, false);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <div className="TopMenu">
+    <div
+      className={cx("TopMenu", {
+        "TopMenu--content": isContentMenu,
+      })}
+      ref={ref}
+    >
       <div className="TopMenu__inner">
         <div className="TopMenu__item">COMMENT S’Y RENDRE?</div>
         <div className="TopMenu__item">COMMENT ÇA MARCHE?</div>
