3dc27b96102caf2f90f9580ce94543034e520d9f
Sergey <sergey@spendesk.com>
2021-07-31 10:22:58 +0200

    first mobile

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/reset.css';
 import '../styles/main.scss';
 import '../styles/content.scss';
 import '../styles/typo.scss';
+import '../styles/menu.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
