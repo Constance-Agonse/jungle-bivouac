32b102aefa2d234870c0c7f6656f86b695cd8146
Sergey <sergey@spendesk.com>
2021-08-05 17:48:27 +0200

    css

--- components/HomeSectionAlternative/HomeSectionAlternative.tsx
+++ components/HomeSectionAlternative/HomeSectionAlternative.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Marquee from "react-double-marquee";
 
+const images = [
+  "https:///i.pinimg.com/564x/dd/59/94/dd5994ca7529d1321275adf59a7e6968.jpg",
+  "https://i.pinimg.com/564x/ec/56/6f/ec566ff385635581b28255c9d8943311.jpg",
+  "https://i.pinimg.com/564x/01/83/69/018369f3ff6dd050b8b653a41007cee5.jpg",
+];
+
 export const HomeSectionAlternative = () => {
+  const imageUrl = images[Math.floor(Math.random()*images.length)];
   return (
     <div className="HomeSectionAlternative">
       <div className="HomeSectionAlternative__bg">
-        <div className="HomeSectionAlternative__left-image" />
+        <div
+          className="HomeSectionAlternative__left-image"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        />
         <div className="HomeSectionAlternative__right-menu">
           <div className="HomeSectionAlternative__right-menu__item">
             27 Août
