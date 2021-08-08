6c40154e7a9de4c6ec99ee9f0d45bcade45e8a29
Constance <constance@MacBook-Pro-de-Constance.local>
2021-08-08 15:56:29 +0200

    updated sliding bar

--- components/CostumeSlider/CostumeSlider.tsx
+++ components/CostumeSlider/CostumeSlider.tsx
@@ -6,24 +6,32 @@ type Props = {
 
 export const CostumeSlider = ({ imageUrls }: Props) => {
   const ref = useRef(null);
-  let xPosition = 0;
+  const itemWidth = 70;
+  const rowWidth = itemWidth * imageUrls.length;
 
-  useEffect(() => {
+  const runAnimation = () => {
+    let xPosition = 0;
     let interval;
     if (ref.current) {
       const interval = setInterval(() => {
+        if (Math.abs(xPosition) >= rowWidth) {
+          xPosition = 0;
+        }
         ref.current.style = `transform: translate(${xPosition--}px, 0)`;
-
       }, 10);
     }
-
     return () => { clearInterval(interval) }
+  }
+
+  useEffect(() => {
+    runAnimation();
   }, []);
 
   const imageElements = imageUrls.map((imageUrl) => {
     return (
-      <img key={imageUrl} src={imageUrl} />
-
+      <div className="CostumeSlider__image-item" style={{ width: itemWidth }}>
+        <img key={imageUrl} src={imageUrl} />
+      </div>
     )
   });
 
@@ -31,9 +39,6 @@ export const CostumeSlider = ({ imageUrls }: Props) => {
     <div className="CostumeSlider">
 
       <div className="wrapper" ref={ref}>
-        {imageElements}
-        {imageElements}
-        {imageElements}
         {imageElements}
         {imageElements}
         {imageElements}
