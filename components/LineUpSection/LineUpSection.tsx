f3a7b63d44e27f079420ab5ed08e79f3da63619e
Constance <constance@MacBook-Pro-de-Constance.local>
2021-08-09 20:00:01 +0200

    lineup

--- components/LineUpSection/LineUpSection.tsx
+++ components/LineUpSection/LineUpSection.tsx
@@ -1,44 +1,47 @@
 import React from "react";
 
+const djs = [
+  {
+    name: 'Lou Ravi',
+    scLink: undefined
+  }, {
+    name: 'Walid',
+    scLink: undefined
+  },
+  {
+    name: 'Paul-Sinh',
+    scLink: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/289925252&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  },
+  {
+    name: 'Human Connections',
+    scLink: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/889885666&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  },
+]
+
 export const LineUpSection = () => {
   return (
     <div className="LineUpSection">
-      <div className="LineUpSection__lineup">
-
-        <div className="LineUpSection__lineup__dj">
-          <div className="LineUpSection__lineup__name">
-            Paul-Sinh
-          </div>
-          <div className="LineUpSection__lineup__sc">
-            <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/289925252&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-          </div>
+      <div className="LineUpSection__content">
+        <div className="LineUpSection__title">
+          Lineup
         </div>
-
-        <div className="LineUpSection__lineup__dj">
-          <div className="LineUpSection__lineup__name">
-            Human Connections
-          </div>
-          <div className="LineUpSection__lineup__sc">
-            <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/889885666&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-
-
+        <div className="LineUpSection__lineup">
+          {djs.map((dj) => (
             <div className="LineUpSection__lineup__dj">
               <div className="LineUpSection__lineup__name">
-                Lou Ravi
+                {dj.name}
               </div>
-
+              {dj.scLink && (
+                <div className="LineUpSection__lineup__sc">
+                  <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src={dj.scLink}></iframe>
+                </div>
+              )}
             </div>
-            <div className="LineUpSection__lineup__dj">
-              <div className="LineUpSection__lineup__name">
-                Walid
-              </div>
-
-            </div>
-          </div>
-          <div className="LineUpSection__background">
-            <div className="LineUpSection__background__img" />
-          </div>
+          ))}
         </div>
+        {/* <div className="LineUpSection__background">
+        <div className="LineUpSection__background__img" />
+      </div> */}
       </div>
     </div>)
 }
